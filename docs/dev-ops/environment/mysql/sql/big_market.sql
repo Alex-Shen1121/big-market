7138e151390e1b6662c8c9e6ad04d34e5d76b5a5
AlexShen <73343630+Alex-Shen1121@users.noreply.github.com>
2024-03-03 22:51:27 +0800

    feat: redis 部署 + 单元测试

--- docs/dev-ops/environment/mysql/sql/big_market.sql
+++ docs/dev-ops/environment/mysql/sql/big_market.sql
@@ -17,6 +17,9 @@
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
+CREATE database if NOT EXISTS `big_market` default character set utf8mb4 collate utf8mb4_0900_ai_ci;
+use `big_market`;
+
 -- ----------------------------
 -- Table structure for award
 -- ----------------------------
